12f35eda70502ca37180535a195419d1a6641a70
aprdev <jonasfg1319@hotmail.com>
2020-06-15 12:09:44 -0300

    Department entity class

---
+++ src/aplication/Program.java
@@ -0,0 +1,15 @@
+package aplication;
+
+import model.entites.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Department dp = new Department(3,"Guill");
+       
+       System.out.println(dp);
+       
+	}
+
+}
