7a265e5e6aa55c48ed7993911e690f43a5101548
aprdev <jonasfg1319@hotmail.com>
2020-06-15 01:09:04 -0300

    Project created

---
+++ src/db/DBExcepiton.java
@@ -0,0 +1,16 @@
+package db;
+
+public class DBExcepiton extends RuntimeException{
+
+   
+	
+	public DBExcepiton(String msg) {
+		super(msg);
+		
+	}
+
+	private static final long serialVersionUID = 1L;
+
+	
+	
+}
