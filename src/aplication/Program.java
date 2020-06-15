adef3ca9da140e977a5c124c22e452c48e5935b4
aprdev <jonasfg1319@hotmail.com>
2020-06-15 15:24:02 -0300

    Seller entity class

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,14 +1,19 @@
 package aplication;
 
+import java.util.Date;
+
 import model.entites.Department;
+import model.entites.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        Department dp = new Department(3,"Guill");
+       Seller sl = new Seller(21,"joao","joao@gmail.com",new Date(),1200.00,dp);
        
-       System.out.println(dp);
+       
+       System.out.println(sl);
        
 	}
 
